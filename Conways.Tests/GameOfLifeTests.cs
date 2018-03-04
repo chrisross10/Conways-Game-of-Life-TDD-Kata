6deff6aad9875402dc472ee6bf9d49f101da4004
Chris Ross <chrisross10@hotmail.com>
2018-03-04 14:11:27 +0000

    Add a guiding test for the "blinker pattern"
    * This pattern deals with most of the rules: births, deaths and survivals. Probably a good test to help drive out some of those rules

--- Conways.Tests/GameOfLifeTests.cs
+++ Conways.Tests/GameOfLifeTests.cs
@@ -14,5 +14,16 @@ namespace Conways.Tests
             var nextGeneration = game.Next();
             Assert.Equal(new List<int[]>(), nextGeneration);
         }
+
+        [Fact(Skip = "Guiding test")]
+        public void It_generates_the_blinker_patter()
+        {
+            var seed = new List<int[]> { new[] { 1, 0 }, new[] { 1, 1 }, new[] { 1, 2 } };
+            var game = new GameOfLife(seed);
+            var secondGeneration = game.Next();
+            var thirdGeneration = game.Next();
+            Assert.Equal(new List<int[]> { new[] { 0, 1 }, new[] { 1, 1 }, new[] { 2, 1 } }, secondGeneration);
+            Assert.Equal(new List<int[]> { new[] { 1, 0 }, new[] { 1, 1 }, new[] { 1, 2 } }, thirdGeneration);
+        }
     }
 }
